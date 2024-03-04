5a4e78e8ef974dfc34c870120dac66fa546a72cb
yogadinugroho <yogaadi.work@gmail.com>
2024-03-04 12:06:55 +0700

    fixing skeleton

--- src/script.js
+++ src/script.js
@@ -19,44 +19,51 @@ function getDataMovie(url, succes, error) {
 // begitu prompt
 // skeleton
 
-const movie = prompt("Masukkan Nama Film: ");
+// const movie = prompt("Masukkan Nama Film: ");
 // setTimeout(() => {
 //   const skeleton = document
 //     .querySelector(".skeleton")
 //     .classList.remove("hidden");
 // }, 1000);
+const movie = "avengers";
 
 getDataMovie(
   "http://www.omdbapi.com/?apikey=dca61bcc&s=" + movie,
   (results) => {
-    const skeletonWrapper = document.querySelector(".skeleton-wrapper");
+    // Skeleton
+    const skeletonWrapper = document.querySelector(".skeleton");
 
-    skeletonWrapper.innerHTML += `<div class="skeleton px-28 flex animate-pulse">
-    <div class="flex-shrink-0">
-      <span class="size-12 block bg-gray-200 rounded-full"></span>
-    </div>
-
-    <div class="ms-4 mt-2 w-full">
-      <h3 class="h-4 bg-gray-200 rounded-full" style="width: 40%"></h3>
-
-      <ul class="mt-5 space-y-3">
-        <li class="w-full h-4 bg-gray-200 rounded-full"></li>
-        <li class="w-full h-4 bg-gray-200 rounded-full"></li>
-        <li class="w-full h-4 bg-gray-200 rounded-full"></li>
-        <li class="w-full h-4 bg-gray-200 rounded-full"></li>
-      </ul>
-    </div>
-  </div>`;
-    // console.log(JSON.parse(results));
-    // olah hasilnya
+    for (let i = 0; i < 10; i++) {
+      skeletonWrapper.innerHTML += `<div
+        class="flex animate-pulse flex-col rounded-2xl bg-slate-500 w-72 hover:scale-105 hover:transition hover:ease-in-out hover:border-green-800"
+      >
+        <div class="rounded-t-2xl h-80 bg-gray-100"></div>
+        <div class="p-4 md:p-5">
+          <div class="h-[85px]">
+            <h3 class="h-4 bg-gray-100 rounded-full" style="width: 100%"></h3>
+          </div>
+          <p class="h-4 bg-gray-100 rounded-full" style="width: 20%"></p>
+          <a
+            class="mt-2 py-2 px-3 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-gray-200 text-gray-200 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+            href="#"
+          >
+            Selengkapnya
+          </a>
+        </div>
+      </div>`;
+    }
 
     setTimeout(() => {
       const cardWrapper = document.querySelector(".card-wrapper");
+      const skeletonWrapper = document
+        .querySelector(".skeleton")
+        .classList.add("hidden");
       const resultParsing = JSON.parse(results);
       const dataResult = resultParsing.Search;
+      //   Real Data
       dataResult.forEach((data) => {
         cardWrapper.innerHTML += `<div
-        class="flex flex-col border shadow-sm rounded-2xl drop-shadow-2xl bg-slate-900 border-gray-700 shadow-slate-700/[.7] w-72 hover:scale-105 hover:transition hover:ease-in-out hover:border-green-800 "
+        class="flex flex-col border shadow-sm rounded-2xl drop-shadow-2xl bg-slate-900 border-gray-700 shadow-slate-700/[.7] w-72 hover:scale-105 hover:transition hover:ease-in-out hover:border-green-800"
       >
       <div class="rounded-t-2xl h-80 bg-slate-200" style="background-image: url(${data.Poster})">
       </div>
@@ -78,7 +85,7 @@ getDataMovie(
         </div>
       </div>`;
       });
-    }, 5000);
+    }, 3000);
   },
   () => {
     console.log("Data tidak ditemukan");
